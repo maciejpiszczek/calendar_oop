d2addc88d38bc228a86113aadbffdfbbbfa9ed13
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-05-16 12:09:45 +0200

    Event class prototype - corrected

--- event.py
+++ event.py
@@ -1,9 +1,9 @@
 class Event:
-    def __int__(self, title, location, start_time, duration, owner, participants):
+    def __init__(self, title, location, start_time, duration, owner, participants):
         self.title = title
         self.location = location
         self.start_time = start_time
         self.duration = duration
         self.owner = owner
         self.participants = participants
-        
+
