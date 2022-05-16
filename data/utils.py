ca0193b3995d3351c012ff21505a16c201cf6111
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-05-16 18:56:47 +0200

    add json data for calendar

--- data/utils.py
+++ data/utils.py
@@ -1,3 +1,4 @@
+import json
 import random
 from datetime import datetime, timedelta
 
@@ -11,7 +12,7 @@ class EventGenerator:
         self.users = ["Ala", "Lech", "Roch", "Damian", "Izmael"]
         self.events = []
 
-    def generate(self, quantity=100):
+    def generate_events(self, quantity=100):
         for _ in range(quantity):
             event = Event(
                 title=random.choice(self.titles),
@@ -22,3 +23,29 @@ class EventGenerator:
                 participants=random.choices(self.users, k=random.randint(0, len(self.users)))
             )
             self.events.append(event)
+
+    def generate_data(self, quantity=100):
+        events_data = []
+
+        for _ in range(quantity):
+            event = {
+                "title": random.choice(self.titles),
+                "location": random.choice(self.locations),
+                "start_time": f'{(datetime.now() + timedelta(minutes=random.randint(50, 500000))):%d-%m-%Y %H:%M}',
+                "duration": random.randint(16, 599),
+                "owner": random.choice(self.users),
+                "participants": random.choices(self.users, k=random.randint(0, len(self.users)))
+            }
+            events_data.append(event)
+
+        return events_data
+
+    def save(self, path):
+        with open(path, 'w') as file:
+            data = json.dumps(self.generate_data(500), indent=4)
+            file.write(data)
+
+    @staticmethod
+    def load(path):
+        with open(path, 'r') as file:
+            return json.load(file)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pprint import pprint as pp
 from data.utils import EventGenerator
 from custom_calendar import Calendar
+from event import Event
 
 data = EventGenerator()
-data.generate(500)
+# data.generate_events(500)
+# data.save('./data.json')
+events_data = data.load('./data.json')
+# pp(events_data)
 
-c = Calendar(data.events)
+events = [Event(**event) for event in events_data]
 
+c = Calendar(events)
 result = c.get_events_by_date('22-05-2022 00:00')
+print(result, len(result))
 # c.remove_event('20-05-2022 16:00')
-
-pp(result)
+#
+# pp(result)
